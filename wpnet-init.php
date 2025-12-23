40ea8b023222b454b8d66976e5176678dc85376f
Gavin Botica <gbotica@gmail.com>
2025-12-23 18:58:43 +1300

    more style fixes

--- wpnet-init.php
+++ wpnet-init.php
@@ -4,7 +4,7 @@
  * Description: Initialise the WP NET mu-plugin library which connects WordPress to WP NET client management services, loads additional plugins, implements various tweaks and creates the WP NET Dashboard Widgets. If you remove this plugin it will be automatically reinstalled during routine maintenance.
  * Author: WP NET
  * Author URI: https://wpnet.nz
- * Version: 1.6.10
+ * Version: 1.6.11
  * Requires at least: 5.8
  * Requires PHP: 7.4
  */
@@ -358,10 +358,10 @@ if ( is_admin() || is_network_admin() ) {
 					display: flex;
 					justify-content: space-between;
 					align-items: center;
-					padding: 0 12px;
+					padding: 8px 12px;
 					background: #f6f7f7;
 					border-radius: 4px;
-					min-height: 38px;
+					
 				}
 				
 				#site_info_widget .wpnet-info-item .label {
@@ -401,7 +401,7 @@ if ( is_admin() || is_network_admin() ) {
 				
 				#site_info_widget .wpnet-disk-details .wpnet-info-item {
 					background: #fff;
-					min-height: 36px;
+					padding: 8px 12px;
 				}
 				
 				#site_info_widget .wpnet-disk-details .note {
